64ff8823e99d7f512d8e8c785e6348f4d44903da
SHANKAR JADAV <shankarjadav360@gmail.com>
2024-04-27 13:48:47 +0530

    changes

--- teraform/ec2.tf
+++ teraform/ec2.tf
@@ -3,13 +3,13 @@ module "ec2_instance" {
 
   name = "single-instance"
 
-  ami = data.aws_ami.ubuntu.id
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.large"
   monitoring             = true
   vpc_security_group_ids = module.vote_service_sg.security_group_id
-  key_name = "jadhav_cred"
+  key_name               = "jadhav_cred"
   subnet_id              = module.vpc.public_subnets
- root_block_device = [
+  root_block_device = [
     {
       volume_size = 30
     }
@@ -29,7 +29,7 @@ module "vote_service_sg" {
 
   description = "Allowing Jenkins, Sonarqube, SSH Access"
 
-  ingress_rules =   [
+  ingress_rules = [
     for port in [22, 443, 8080, 9000, 9090, 80] : {
       from_port        = port
       to_port          = port
@@ -39,7 +39,7 @@ module "vote_service_sg" {
       prefix_list_ids  = []
       security_groups  = []
       self             = false
-    #   description      = "${upper(regex_replace("Access", "", element(["SSH", "HTTPS", "Jenkins", "Sonarqube", "Prometheus", "HTTP"], port / 100)))} Access"
+      #   description      = "${upper(regex_replace("Access", "", element(["SSH", "HTTPS", "Jenkins", "Sonarqube", "Prometheus", "HTTP"], port / 100)))} Access"
     }
   ]
 
