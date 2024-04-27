a180d187bc1d2d10cc703ece336846ef10850c08
SHANKAR JADAV <shankarjadav360@gmail.com>
2024-04-27 11:10:30 +0530

    changes

--- teraform/vpc.tf
+++ teraform/vpc.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name = "main"
   cidr = "10.0.0.0/16"
 
-  azs = data.aws_availability_zones.available
+  azs = var.azs
   private_subnets = ["10.0.0.0/19", "10.0.32.0/19"]
   public_subnets = ["10.0.64.0/19", "10.0.96.0/19"]
 
