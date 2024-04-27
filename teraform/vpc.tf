f62888be6a7f2ac6569aa98eca5e125fb0ceef7f
SHANKAR JADAV <shankarjadav360@gmail.com>
2024-04-27 14:39:42 +0530

    changes

--- teraform/vpc.tf
+++ teraform/vpc.tf
@@ -8,6 +8,7 @@ module "vpc" {
   azs = data.aws_availability_zones.zones.names
   private_subnets = ["10.0.0.0/19", "10.0.32.0/19"]
   public_subnets = ["10.0.64.0/19", "10.0.96.0/19"]
+  map_public_ip_on_launch = true
 
   public_subnet_tags = {
     "3 tier application" : "1"
